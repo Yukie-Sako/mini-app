6e08caf8fd040a5f2c109e421971ec7c23089923
Yukie-Sako <vetkas4astia@gmail.com>
2018-11-08 04:54:03 +0900

    create: blogs_controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,22 +1,48 @@
 class BlogsController < ApplicationController
 
-before_action :move_to_index, except: :index
+before_action :move_to_index, except: [:index, :show]
 
   def index
-    @blogs = Blog.all
+    @blogs = Blog.includes(:user).order("id DESC").page(params[:page]).per(5)
   end
 
   def new
+    @blog = Blog.new
   end
 
   def create
-    Blog.create(title: tweet_params[:title], text: tweet_params[:text], user_id: current_user.id)
+    Blog.create(title: blog_params[:title], image: blog_params[:image], text: blog_params[:text], user_id: current_user.id)
   end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy if blog.user_id == current_user.id
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.user_id == current_user.id
+      @blog.update(params.require(:blog).permit(:title, :text, :image))
+      redirect_to user_path
+    end
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
 
   private
-  def tweet_params
-    params.permit(:title, :text, :image)
+  def blog_params
+    params.require(:blog).permit(:title, :text, :image)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 
 end
